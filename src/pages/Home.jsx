c43bd6003e531fdf7fc3c012e1c677e3f406fb9a
Ejike Onwe <31670361+eiejay@users.noreply.github.com>
2025-11-07 07:58:16 -0600

    Change weather heading text to 'Today we are Done!'

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -70,7 +70,7 @@ const Home = () => {
       <div className="weather-preloader container-fluid d-flex align-items-center flex-column">
         <main className="my-5 preloader-weather-heading">
           <h2 className="text-center text-capitalize m-auto fw-bold fs-2">
-            How's today's weather?
+                Today we are Done!
           </h2>
         </main>
 
